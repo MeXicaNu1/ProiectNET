dae61bcebbcdd692c7b7ee3620e7ad1b076e3865
MeXicaNu <daniel.padurean99@e-uvt.ro>
2021-05-27 00:20:53 +0300

    Test

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace ProiectNET
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTweet = new System.Windows.Forms.Button();
+            this.txtTweet = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imgAttach = new System.Windows.Forms.PictureBox();
+            this.labelImage = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imgAttach)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnTweet
+            // 
+            this.btnTweet.Location = new System.Drawing.Point(596, 180);
+            this.btnTweet.Name = "btnTweet";
+            this.btnTweet.Size = new System.Drawing.Size(109, 39);
+            this.btnTweet.TabIndex = 0;
+            this.btnTweet.Text = "Send Tweet";
+            this.btnTweet.UseVisualStyleBackColor = true;
+            this.btnTweet.Click += new System.EventHandler(this.btnTweet_Click);
+            // 
+            // txtTweet
+            // 
+            this.txtTweet.Location = new System.Drawing.Point(115, 12);
+            this.txtTweet.Multiline = true;
+            this.txtTweet.Name = "txtTweet";
+            this.txtTweet.Size = new System.Drawing.Size(590, 127);
+            this.txtTweet.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Attach image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imgAttach
+            // 
+            this.imgAttach.Location = new System.Drawing.Point(130, 145);
+            this.imgAttach.Name = "imgAttach";
+            this.imgAttach.Size = new System.Drawing.Size(91, 74);
+            this.imgAttach.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgAttach.TabIndex = 3;
+            this.imgAttach.TabStop = false;
+            // 
+            // labelImage
+            // 
+            this.labelImage.AutoSize = true;
+            this.labelImage.Location = new System.Drawing.Point(227, 180);
+            this.labelImage.Name = "labelImage";
+            this.labelImage.Size = new System.Drawing.Size(0, 15);
+            this.labelImage.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(9, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 84);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 238);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelImage);
+            this.Controls.Add(this.imgAttach);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtTweet);
+            this.Controls.Add(this.btnTweet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imgAttach)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTweet;
+        private System.Windows.Forms.TextBox txtTweet;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox imgAttach;
+        private System.Windows.Forms.Label labelImage;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tweetinvi;
+using System.IO;
+namespace ProiectNET
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            Auth.SetUserCredentials("FT8p9znqLRCkcUcADaDXDfwy9", "1xPPnG9URrZl6w2AWBmrcpxSwukbjipQrlMLtvxGoDCbwA6hs7", "1650674666-OI5J2lLU9NHFkbVyfctSgRYLaqNt4A7iD1cxIdx", "ZqzuU76Z2FsnC0JOkE1UCe3FofJfYlHpSY4PUGfnhtLic");
+        }
+        private void Form1_load(object sender, EventArgs e)
+        {
+            var user = User.GetAuthenticatedUser();
+            pictureBox1.ImageLocation = user.ProfileImageUrl;
+            label1.Text = user.Name;
+            label2.Text = "@" + user.ScreenName;
+        }
+        private void btnTweet_Click(object sender, EventArgs e)
+        {
+            if (labelImage.Text != "")
+            {
+                byte[] file = File.ReadAllBytes(imgAttach.ImageLocation);
+                Tweet.PublishTweetWithImage(txtTweet.Text, file);
+            }
+            else
+            
+            {
+                var firstTweet = Tweet.PublishTweet(txtTweet.Text);
+            }
+            MessageBox.Show("Tweet sent!");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            imgAttach.ImageLocation = ofd.FileName;
+            labelImage.Text = ofd.FileName;
+        }
+
+        
+    }
+}
